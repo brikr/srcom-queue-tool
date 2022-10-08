5fcb2642fc9894764f5abf2fcda6f0a0b1605029
Brik <brikr@users.noreply.github.com>
2022-10-08 14:23:21 -0700

    allow preferred copy order

--- src/util/duration.ts
+++ src/util/duration.ts
@@ -21,3 +21,19 @@ export function formatDuration(isoDuration: string): string {
 
   return `${hours}${minutes}${seconds}`;
 }
+
+const SECONDS_MS = 1000;
+const MINUTES_MS = SECONDS_MS * 60;
+const HOURS_MS = MINUTES_MS * 60;
+const DAYS_MS = HOURS_MS * 24;
+
+export function durationToMillis(isoDuration: string): number {
+  const parsedTime = parse(isoDuration);
+
+  const daysMs = parsedTime.days ? parsedTime.days * DAYS_MS : 0;
+  const hoursMs = parsedTime.hours ? parsedTime.hours * HOURS_MS : 0;
+  const minutesMs = parsedTime.minutes ? parsedTime.minutes * MINUTES_MS : 0;
+  const secondsMs = parsedTime.seconds ? parsedTime.seconds * SECONDS_MS : 0;
+
+  return daysMs + hoursMs + minutesMs + secondsMs;
+}
